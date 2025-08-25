6104ef19970a4774de908cf95af12755c390038b
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-08-25 09:49:36 +0200

    added css with :focus and in the form error handling !text return

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function App() {
   const [text, setText] = React.useState("")
   const [list, setList] = React.useState([])
+  const focusHere = React.useRef(null)
 
   const handleText = (e) => {
     setText(e.target.value)
@@ -10,6 +11,10 @@ function App() {
 
   const handleForm = (e) => {
     e.preventDefault()
+    if (!text) {
+      return;
+    }
+    focusHere.current.focus()
     console.log("submitted")
     setList(prevList => [...prevList, text])
     setText("")
@@ -20,8 +25,10 @@ function App() {
       <form onSubmit={handleForm}>
         <h1>List</h1>
         <input
+          type="text"
           value={text}
           onChange={handleText}
+          ref={focusHere}
         ></input>
         <button>Add</button>
       </form>
